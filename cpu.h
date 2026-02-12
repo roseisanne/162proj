5b6f6fdd4a75dcafd96399d5e8918a105bc00b39
roseisanne <shildt.roseanne@gmail.com>
2026-02-12 13:40:05 -0800

    print dat file

--- cpu.h
+++ cpu.h
@@ -1,26 +1,33 @@
 #pragma once
+// struct Data_Mem {
+//     int addr;
+//     int val;
+//     int size;
+    
+//     Data_Mem {
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <stdlib.h>
+//     }
 
-using namespace std;
+// };
 
 
-struct function_unit {
-    bool busy;
-    int operation_type; //0: add, 1: sub, 2: mul, 3: div
-    int destination;
-    int source1;
-    int source2;
-    int address; //or immediate instructions
-};
 
-struct register_station {
+// struct Instr_Mem {
+//     string operator;
+//     int type;
+//     R_type
+//     I_type
+//     S_type
 
-};
+// }
+
+/* 
+struct Reservation_Station {
+
+}
+
+struct Reg_Stat_Table {
+
+}
+
+*/
--- main.cpp
+++ main.cpp
@@ -6,43 +6,37 @@
 #include <fstream>
 #include <sstream>
 #include <stdlib.h>
+#include <map>
 
-#include "file.h"
+#include "CPU.h"
 
 using namespace std;
+//data memory <int, int>
+//instr memory <string>
+
 //file input function
-<vector<<int,int>, string> loadData(const string& filename) {
-    vector<vector<int>> data;
+vector<string> loadData(const string& filename) {
+    vector<string> lines;
     ifstream file(filename);
 
     if(!file.is_open()) {
-        cerr << "Error opening file: " << filename << endl;
-        return;
+        cerr << "Error opening file: " << filename << "\n";
+        return lines;
     }
     //reading
     string line;
-    while(getline(file, line, ',')){
-        if (line.empty()) continue;
-    }
-    //translate
-
-    stringstream ss(line);
-
-    while (ss >>) {
-
+    while(getline(file, line)){
+        lines.push_back(line);
     }
 
     file.close();
-    return data
+    return lines;
 }
 
 main (){
-     data = loadData("data.txt");
-    for (const auto& row : data) {
-        for (const auto& val : row) {
-            cout << val << " ";
-        }
-        cout << endl;
+    vector<string> data = loadData("prog.dat");
+    for (const auto& line: data){
+        cout << line << "\n";
     }
 
     return 0;
