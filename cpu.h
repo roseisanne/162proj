06972bdb2eae762ea5d220256f2fccb44877586a
roseisanne <glaze@fishy.localdomain>
2026-02-26 13:45:49 -0800

    remove readme contents, sperate file contents in program

--- cpu.h
+++ cpu.h
@@ -1,33 +1,61 @@
 #pragma once
-// struct Data_Mem {
-//     int addr;
-//     int val;
-//     int size;
-    
-//     Data_Mem {
+//fetch unit fetched NF=1 instr every cycle
 
-//     }
+//when branch is fetched/decoded, fetch unit stalls until branch instr
+//completes execution in branch unit and correct next PC determined
 
-// };
+//decode unit can only hold NI=1 instr - acts as pipeline latch between
+//fetch and issue, issue stage stalled, decode stage holds instr
+//and signals detch unit to stall
+
+//up to NW=1 instr issued every clk cycle from the instruction queue to
+//free reservation station.
+
+//single common stat bus (nb=1) used to connect functional units to
+//register file and reservation stations
+// - instrs commit as soon as they finish execution and write result to the CDB
+// - if mult functional units finish execution at the same cycle, unit
+//   with the oldest instr gets priority to broadcast result, other
+//   units must stall and wait till next cycle to retry
+
+//implement register renaming using Reservation Station IDs as tags
+// - use register status table: tracks whether register's value is
+//   currently valid in register file or if pending result from specific 
+//   Reservation Station ID
+
+//implement forwarding vis CDB, when result broadcast on CDB, any
+//reservation station waiting for that specific tag must capture value
+//immediatly in same cycle
+
+// assume that the entire program fits in the instruction cache
+// (i.e., it always takes one cycle to read a cache line). Also, 
+// the data cache is perfect (hit rate =100%) and single-ported. Reading 
+// or writing a word always takes one cycle. There are no cache
+// misses to simulate. You may assume a logical separation between 
+// Instruction Memory and Data Memory. Use the addresses specified in the input file for data memory. Instructions are stored in
+// a separate list starting at Index 0. The Program Counter (PC) 
+// simply tracks the index of the instruction in 
+// this list (PC, PC+1, PC+2...)
+
+//keep statistic about
+//total executiion cycles
+//stall events
 
 
 
-// struct Instr_Mem {
-//     string operator;
-//     int type;
-//     R_type
-//     I_type
-//     S_type
+/* data memory
+instr list
+label -> pc map*/
 
-// }
-
-/* 
-struct Reservation_Station {
-
-}
-
-struct Reg_Stat_Table {
-
-}
-
-*/
+struct Instruction 
+{
+    std::string op;
+    //int reg
+    int rd, rs1, rs2;
+    //FP registers
+    int frd, frs1, frs2;
+    //immed
+    int imm;
+    //branch traget
+    int targetPC;
+};
--- main.cpp
+++ main.cpp
@@ -10,22 +10,24 @@
 
 #include "CPU.h"
 
-using namespace std;
 //data memory <int, int>
 //instr memory <string>
 
 //file input function
-vector<string> loadData(const string& filename) {
-    vector<string> lines;
-    ifstream file(filename);
+std::vector<std::string> loadData(const std::string& filename) 
+{
+    std::vector<std::string> lines;
+    std::ifstream file(filename);
 
-    if(!file.is_open()) {
-        cerr << "Error opening file: " << filename << "\n";
+    if(!file.is_open()) 
+    {
+        std::cerr << "Error opening file: " << filename << "\n";
         return lines;
     }
     //reading
-    string line;
-    while(getline(file, line)){
+    std::string line;
+    while(std::getline(file, line))
+    {
         lines.push_back(line);
     }
 
@@ -33,10 +35,49 @@ vector<string> loadData(const string& filename) {
     return lines;
 }
 
-main (){
-    vector<string> data = loadData("prog.dat");
-    for (const auto& line: data){
-        cout << line << "\n";
+//2. handel labels -> map loop to  current instr index, remove label from line and store clean instr
+//3. convert to structured instr
+
+int main ()
+{
+    std::vector<std::string> data = loadData("prog.dat");
+    std::map<int, int> dataMem;
+    std::vector<std::string> instrLines;
+    std::map<std::string, int> labelMap;
+
+    for (const auto& line: data)
+    {
+        std::stringstream ss(line);
+        int addr{};
+        int val{};
+        char comma{};
+        std::string loopLabel;
+
+        //separate mem init and set of instructions
+        if (ss >> addr >> comma >> val && comma == ',')
+        {
+            dataMem[addr] = val;
+        }
+        else 
+        {
+            if (ss >> loopLabel && lopeLable == "loop")
+            {
+                
+            }
+            instrLines.push_back(line);
+        }
+    }
+    //checking
+    std::cout << "Mem contents:\n";
+    for (const auto& pair : dataMem)
+    {
+        std::cout << pair.first << " - " << pair.second << "\n";
+    }
+
+    std::cout << "Instr contents:\n";
+    for (int i = 0; i < instrLines.size(); i++)
+    {
+        std::cout << instrLines.at(i) << "\n";
     }
 
     return 0;
